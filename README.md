5e428c2f2055b854fb2033d2aa3835c5ca4f53aa
Eray Erdin <eraygezer.94@gmail.com>
2021-05-20 12:53:36 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LevelHead Browser
+
+An Android application to browse the world of LevelHead.
---
+++ android/app/src/main/kotlin/io/github/erayerdin/levelheadbrowser/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.erayerdin.levelheadbrowser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
