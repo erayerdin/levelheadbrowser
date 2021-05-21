c20fe3f2154111fd7eee158054a704ab68d2e381
Eray Erdin <eraygezer.94@gmail.com>
2021-05-21 10:12:19 +0300

    set up routes and created scaffold home page

---
+++ lib/presentation/pages/home/home.page.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Eray Erdin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LevelHead Browser'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
