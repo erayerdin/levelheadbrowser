9df87e935ddddf35ba162dd10c62f2fbe4186ab9
Eray Erdin <eraygezer.94@gmail.com>
2021-05-21 07:43:35 +0300

    added rumpus map to profile converter

---
+++ lib/data/converters/profile.dart
@@ -0,0 +1,49 @@
+// Copyright (c) 2021 Eray Erdin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import 'dart:convert';
+
+import 'package:levelheadbrowser/data/models/profile.dart';
+import 'package:levelheadbrowser/di.dart';
+import 'package:logger/logger.dart';
+
+class RumpusMapToProfileConverter
+    extends Converter<Map<String, dynamic>, Profile> {
+  final Logger _logger = getIt.get();
+
+  @override
+  Profile convert(Map<String, dynamic> input) {
+    _logger.d('Converting LevelHead profile data to Profile object...');
+    _logger.v('input: $input');
+
+    return Profile(
+      id: input['userId'],
+      dateJoined: DateTime.parse(input['createdAt']),
+      alias: input['alias']['alias'],
+      avatar: input['alias']['avatarId'],
+      stats: ProfileStats(
+        subscriberCount: input['stats']['Subscribers'],
+        followingCount: input['stats']['NumFollowing'],
+        crownCount: input['stats']['Crowns'],
+        shoeCount: input['stats']['Shoes'],
+        playtimeSeconds: input['stats']['PlayTime'],
+        tipsCount: TipStats(
+          perLevel: input['stats']['TipsPerLevel'],
+          perDay: input['stats']['TipsPerDay'],
+        ),
+        tippedCount: TipStats(
+          perLevel: input['stats']['TippedPerLevelPlayed'],
+          perDay: input['stats']['TippedPerDay'],
+        ),
+        hiddenGemCount: input['stats']['HiddenGem'],
+        trophyCount: input['stats']['Trophies'],
+        perkPoints: input['stats']['PerkPoints'],
+        campaignProgress: input['stats']['CampaignProg'],
+        timeTrophyCount: input['stats']['TimeTrophies'],
+      ),
+    );
+  }
+}
--- lib/di.dart
+++ lib/di.dart
@@ -4,11 +4,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import 'dart:convert';
+
 import 'package:get_it/get_it.dart';
+import 'package:levelheadbrowser/data/converters/profile.dart';
 import 'package:logger/logger.dart';
 
 final getIt = GetIt.instance;
 
 void setUpDI() async {
   getIt.registerLazySingleton(() => Logger());
+
+  getIt.registerLazySingleton<Converter>(() => RumpusMapToProfileConverter(),
+      instanceName: 'data.converters.profile.toProfile.fromRumpusMap');
 }
