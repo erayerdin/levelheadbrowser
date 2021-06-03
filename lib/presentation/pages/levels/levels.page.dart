b4b31d20513889fb3252fbd6ce6d7e7c413c7831
Eray Erdin <eraygezer.94@gmail.com>
2021-06-03 13:36:26 +0300

    added wip bloc to levels page

--- lib/presentation/pages/levels/levels.page.dart
+++ lib/presentation/pages/levels/levels.page.dart
@@ -5,13 +5,29 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:levelheadbrowser/logic/levels/levels_bloc.dart';
 
 class LevelsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Hello from Levels page.'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (blocCtx) => LevelsBloc()..add(LoadLevelsEvent()))
+      ],
+      child: Container(
+        child: BlocBuilder<LevelsBloc, LevelsState>(
+          builder: (context, state) {
+            if (state is LoadingLevelsState) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is FailedLoadingLevelsState) {
+              return Center(child: Text(state.message));
+            }
+            return Center(
+              child: Text('Hello from Levels page.'),
+            );
+          },
+        ),
       ),
     );
   }
