54bf0bcc09672faa0cd2aa8000f1f5dac3a4171a
Eray Erdin <eraygezer.94@gmail.com>
2021-06-04 11:44:59 +0300

    added gesture detector to level card

--- lib/presentation/pages/levels/components/levelcard.component.dart
+++ lib/presentation/pages/levels/components/levelcard.component.dart
@@ -40,76 +40,86 @@ class LevelCardComponent extends StatelessWidget {
             ),
       ),
       child: Builder(
-        builder: (context) => Card(
-          color: Color.lerp(Colors.red, Colors.green, level.stats.clearRate),
-          child: Container(
-            margin: _margin,
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        CachedNetworkImage(
-                          imageUrl: level.getAvatarURL(),
-                          placeholder: (context, url) =>
-                              CircularProgressIndicator(),
-                          errorWidget: (context, url, error) =>
-                              Icon(Icons.error),
-                        ),
-                        SizedBox(width: 10),
-                        SizedBox(
-                          width: 170,
-                          child: Text(
-                            level.title,
-                            style: Theme.of(context).textTheme.subtitle1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 120,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        builder: (context) => GestureDetector(
+          onTap: () {
+            // TODO implement card press
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Not Implemented Yet'),
+              ),
+            );
+          },
+          child: Card(
+            color: Color.lerp(Colors.red, Colors.green, level.stats.clearRate),
+            child: Container(
+              margin: _margin,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
                         children: [
-                          Row(
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.favorite_border),
-                                onPressed: () {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                      content: Text('Not Implement Yet'),
-                                    ),
-                                  );
-                                }, // TODO implement like button
-                              ),
-                              Text(level.stats.likeCount.toString()),
-                            ],
+                          CachedNetworkImage(
+                            imageUrl: level.getAvatarURL(),
+                            placeholder: (context, url) =>
+                                CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
                           ),
-                          Row(
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.star_outline_outlined),
-                                onPressed: () {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                      content: Text('Not Implement Yet'),
-                                    ),
-                                  );
-                                }, // TODO implement favorite button
-                              ),
-                              Text(level.stats.favoriteCount.toString()),
-                            ],
+                          SizedBox(width: 10),
+                          SizedBox(
+                            width: 170,
+                            child: Text(
+                              level.title,
+                              style: Theme.of(context).textTheme.subtitle1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
                         ],
                       ),
-                    )
-                  ],
-                )
-              ],
+                      SizedBox(
+                        width: 120,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.favorite_border),
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('Not Implement Yet'),
+                                      ),
+                                    );
+                                  }, // TODO implement like button
+                                ),
+                                Text(level.stats.likeCount.toString()),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.star_outline_outlined),
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('Not Implement Yet'),
+                                      ),
+                                    );
+                                  }, // TODO implement favorite button
+                                ),
+                                Text(level.stats.favoriteCount.toString()),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
