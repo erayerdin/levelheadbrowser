591882fff30b9426aff249bbcd4be1954a6bcc35
Eray Erdin <eraygezer.94@gmail.com>
2021-06-07 13:39:05 +0300

    added link component

---
+++ lib/presentation/components/link/link.component.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 Eray Erdin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LinkComponent extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Function()? onTap;
+
+  const LinkComponent(
+    this.text, {
+    Key? key,
+    this.color = Colors.blue,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: text,
+        style: TextStyle(color: color),
+        recognizer: TapGestureRecognizer()..onTap = onTap,
+      ),
+    );
+  }
+}
