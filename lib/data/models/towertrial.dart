57a949cec0399e69bcf8009bb7645ccc6f3763f3
Eray Erdin <eraygezer.94@gmail.com>
2021-06-10 11:45:15 +0300

    implemented TowerTrial model

--- lib/data/models/towertrial.dart
+++ lib/data/models/towertrial.dart
@@ -4,5 +4,24 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// TODO implement TowerTrial
-class TowerTrial {}
+import 'package:equatable/equatable.dart';
+
+import 'package:levelheadbrowser/data/models/level.dart';
+import 'package:levelheadbrowser/data/models/record.dart';
+
+class TowerTrial extends Equatable {
+  final DateTime createdAt;
+  final List<Level> levels;
+  final List<Record> fastestTimeRecords;
+  final List<Record> highScoreRecords;
+
+  TowerTrial({
+    required this.createdAt,
+    required this.levels,
+    required this.fastestTimeRecords,
+    required this.highScoreRecords,
+  });
+
+  @override
+  List<Object?> get props => [createdAt];
+}
