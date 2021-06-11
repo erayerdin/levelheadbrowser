a873ec3344a41f56399d7f3f135f0e3837aeecd1
Eray Erdin <eraygezer.94@gmail.com>
2021-06-11 16:26:18 +0300

    wip: implemented tower trials page

--- lib/presentation/pages/towertrials/towertrials.page.dart
+++ lib/presentation/pages/towertrials/towertrials.page.dart
@@ -5,13 +5,42 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:levelheadbrowser/logic/towertrials/towertrials_bloc.dart';
+import 'package:levelheadbrowser/presentation/components/filterpanel/filterpanel.component.dart';
+import 'package:levelheadbrowser/presentation/pages/levels/components/levelcard.component.dart';
 
 class TowerTrialsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Hello from Tower Trials page.'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (blocCtx) => TowerTrialsBloc()
+            ..add(
+              LoadTowerTrialsEvent(),
+            ),
+        ),
+      ],
+      child: BlocBuilder<TowerTrialsBloc, TowerTrialsState>(
+        builder: (context, state) {
+          if (state is LoadedTowerTrialsState) {
+            return FilterPanel(
+              body: ListView(
+                children: state.towerTrial.levels
+                    .map((e) => LevelCardComponent(level: e))
+                    .toList(),
+              ),
+              formHeader: Text(''), // TODO add form header
+              formChildren: [], // TODO add form children
+              onApply: (form) {}, // TODO implement form onApply
+            );
+          } else if (state is FailedLoadingTowerTrialsState) {
+            return Center(child: Text(state.message));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
