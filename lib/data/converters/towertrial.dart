a8ed961f1f6ac90900f809b87cb68f0afec4d67b
Eray Erdin <eraygezer.94@gmail.com>
2021-06-11 13:13:18 +0300

    added RumpusMapToTowerTrialConverter
    Squashed commit of the following:
    
    commit 10d1c52be3f93e78eb6afe0c118bed3c6de45422
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Fri Jun 11 13:11:58 2021 +0300
    
        implemented _buildRecords
    
    commit 5f8a54ae867598c24590092315f5d0b55b85e5e2
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Fri Jun 11 13:00:26 2021 +0300
    
        implemented _buildLevels
    
    commit 21bdbe2d40592399f23f6371c88af669403e718c
    Author: Eray Erdin <eraygezer.94@gmail.com>
    Date:   Thu Jun 10 11:54:49 2021 +0300
    
        added wip RumpusMapTowerTrialConverter

--- lib/data/converters/towertrial.dart
+++ lib/data/converters/towertrial.dart
@@ -6,13 +6,78 @@
 
 import 'dart:convert';
 
+import 'package:levelheadbrowser/data/models/level.dart';
+import 'package:levelheadbrowser/data/models/params/levels.dart';
+import 'package:levelheadbrowser/data/models/params/players.dart';
+import 'package:levelheadbrowser/data/models/profile.dart';
+import 'package:levelheadbrowser/data/models/record.dart';
 import 'package:levelheadbrowser/data/models/towertrial.dart';
+import 'package:levelheadbrowser/data/repositories/level.dart';
+import 'package:levelheadbrowser/data/repositories/profile.dart';
+import 'package:levelheadbrowser/di.dart';
+import 'package:logger/logger.dart' show Logger;
 
 class RumpusMapToTowerTrialConverter
     extends Converter<Map<String, dynamic>, Future<TowerTrial>> {
+  final Logger _logger = getIt.get();
+  final LevelRepository<LevelsParams, Level> _levelRepository =
+      getIt.get(instanceName: 'data.repositories.level.rumpus');
+  final ProfileRepository<PlayersParams, Profile> _profileRepository =
+      getIt.get(instanceName: 'data.repositories.profile.rumpus');
+
+  Future<List<Level>> _buildLevels(Map<String, dynamic> input) async {
+    _logger.d('Building levels...');
+
+    List<String> levelIds = input['levelIds'];
+    // REF https://stackoverflow.com/a/38016334/2926992
+    List<Level> levels = await _levelRepository.list(
+      LevelsParams(
+        ids: levelIds.toSet(),
+      ),
+    );
+    levels.sort((l1, l2) => levelIds.indexOf(l1.id) - levelIds.indexOf(l2.id));
+
+    return levels;
+  }
+
+  Future<List<Record>> _buildRecords(
+      Map<String, dynamic> input, bool isFastestTime) async {
+    _logger.d('Building records...');
+
+    List<Map<String, dynamic>> recordsData = isFastestTime
+        ? input['records']['FastestTime']
+        : input['records']['HighScore'];
+
+    List<Profile> profiles = await _profileRepository.list(
+      PlayersParams(
+        ids: recordsData.map((e) => e['userId'] as String).toSet(),
+      ),
+    );
+
+    List<Record> records = recordsData
+        .map(
+          (e) => Record(
+            profile:
+                profiles.firstWhere((element) => element.id == e['userId']),
+            createdAt: DateTime.parse(e['createdAt']),
+            value: e['value'],
+          ),
+        )
+        .toList();
+
+    return records;
+  }
+
   @override
-  Future<TowerTrial> convert(Map<String, dynamic> input) {
-    // TODO: implement convert
-    throw UnimplementedError();
+  Future<TowerTrial> convert(Map<String, dynamic> input) async {
+    _logger.d('Converting Levelhead tower trial data to TowerTrial object...');
+    _logger.v('input: $input');
+
+    return TowerTrial(
+      createdAt: DateTime.parse(input['createdAt']),
+      levels: await _buildLevels(input),
+      fastestTimeRecords: await _buildRecords(input, true),
+      highScoreRecords: await _buildRecords(input, false),
+    );
   }
 }
