59573eb112f91bbd0b6b8f159e770926e3e35684
Eray Erdin <eraygezer.94@gmail.com>
2021-06-16 17:18:43 +0300

    added update method to SettingsProvider

--- lib/data/providers/settings.dart
+++ lib/data/providers/settings.dart
@@ -8,18 +8,26 @@ import 'package:hive/hive.dart';
 import 'package:levelheadbrowser/di.dart';
 import 'package:logger/logger.dart';
 
-abstract class SettingsProvider<ParamModel, ResponseType> {
-  Future<ResponseType> get(ParamModel params);
+abstract class SettingsProvider<ParamModel, Type> {
+  Future<Type> get(ParamModel params);
+  Future<void> update(Type instance);
 }
 
 class HiveSettingsProvider
     implements SettingsProvider<dynamic, Map<String, dynamic>> {
+  static const BOX_KEY = 'settings';
   final Logger _logger = getIt.get();
   final Future<Box> _box = getIt.getAsync(instanceName: 'hive.box.settings');
 
   @override
   Future<Map<String, dynamic>> get(params) async {
     _logger.d('Getting settings using Hive Settings Provder...');
-    return (await _box).get('settings');
+    return (await _box).get(BOX_KEY);
+  }
+
+  @override
+  Future<void> update(Map<String, dynamic> instance) async {
+    _logger.d('Updating settings using Hive Settings Provider...');
+    await (await _box).put(BOX_KEY, instance);
   }
 }
