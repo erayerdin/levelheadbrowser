56194f2257da0aad7333704a7adf7b4adf3f4ce3
Eray Erdin <eraygezer.94@gmail.com>
2021-06-23 14:38:04 +0300

    implement initial value and onSubmitted for misc

--- lib/presentation/pages/settings/pages/card/profile/misc.part.dart
+++ lib/presentation/pages/settings/pages/card/profile/misc.part.dart
@@ -5,16 +5,29 @@ class _MiscOptionsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SettingsBloc settingsBloc = BlocProvider.of<SettingsBloc>(context);
+
     return SectionComponent(
       label: 'Miscellaneous',
       children: [
         FormBuilderTextField(
           name: '',
-          initialValue: '<no-alias>',
+          initialValue: settingsBloc.settings.card.profileCard.noAliasDefault,
           autovalidateMode: AutovalidateMode.always,
           validator: FormBuilderValidators.compose([
             FormBuilderValidators.required(context),
           ]),
+          onSubmitted: (val) => settingsBloc.add(
+            UpdateSettingsEvent(
+              settings: settingsBloc.settings.copyWith(
+                card: settingsBloc.settings.card.copyWith(
+                  profileCard: settingsBloc.settings.card.profileCard.copyWith(
+                    noAliasDefault: val,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ],
     );
