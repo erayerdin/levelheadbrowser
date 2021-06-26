4c32205bd88bdcf009b379b166efcdbf5574ae59
Eray Erdin <eraygezer.94@gmail.com>
2021-06-26 19:52:39 +0300

    added max threshold info to level card interpolation field settings widget

--- lib/presentation/pages/settings/pages/card/level/interpolation.part.dart
+++ lib/presentation/pages/settings/pages/card/level/interpolation.part.dart
@@ -10,30 +10,43 @@ class _InterpolationOptionsSection extends StatelessWidget {
     return SectionComponent(
       label: 'Color Interpolation Options',
       children: [
-        FormBuilderDropdown<LevelCardColorInterpolationField>(
-          name: 'Color Interpolation Options',
-          decoration: InputDecoration(labelText: 'Interpolation Field'),
-          items: LevelCardColorInterpolationField.values
-              .map(
-                (e) => DropdownMenuItem(
-                  child: Text(e.toRepr()),
-                  value: e,
+        BlocBuilder<SettingsBloc, SettingsState>(
+          builder: (context, state) {
+            if (state is LoadedSettingsState) {
+              return FormBuilderDropdown<LevelCardColorInterpolationField>(
+                name: 'Color Interpolation Options',
+                decoration: InputDecoration(
+                  labelText: 'Interpolation Field',
+                  helperText:
+                      'Maximum Predefined Threshold: ${state.settings.card.levelCard.colorInterpolationField.max}',
                 ),
-              )
-              .toList(),
-          initialValue:
-              settingsBloc.settings.card.levelCard.colorInterpolationField,
-          onChanged: (field) => settingsBloc.add(
-            UpdateSettingsEvent(
-              settings: settingsBloc.settings.copyWith(
-                card: settingsBloc.settings.card.copyWith(
-                  levelCard: settingsBloc.settings.card.levelCard.copyWith(
-                    colorInterpolationField: field,
+                items: LevelCardColorInterpolationField.values
+                    .map(
+                      (e) => DropdownMenuItem(
+                        child: Text(e.toRepr()),
+                        value: e,
+                      ),
+                    )
+                    .toList(),
+                initialValue: settingsBloc
+                    .settings.card.levelCard.colorInterpolationField,
+                onChanged: (field) => settingsBloc.add(
+                  UpdateSettingsEvent(
+                    settings: settingsBloc.settings.copyWith(
+                      card: settingsBloc.settings.card.copyWith(
+                        levelCard:
+                            settingsBloc.settings.card.levelCard.copyWith(
+                          colorInterpolationField: field,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
+              );
+            }
+
+            return SizedBox();
+          },
         )
       ],
     );
