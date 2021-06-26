340d31136d56d73970c4b5af0aea3363fee57dce
Eray Erdin <eraygezer.94@gmail.com>
2021-06-26 20:13:22 +0300

    wip: implemented profile form appearance settings content

--- lib/presentation/pages/settings/pages/form/profile/profile.settings.page.dart
+++ lib/presentation/pages/settings/pages/form/profile/profile.settings.page.dart
@@ -5,6 +5,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class ProfileFormSettingsDialog extends StatelessWidget {
   const ProfileFormSettingsDialog({Key? key}) : super(key: key);
@@ -13,6 +14,25 @@ class ProfileFormSettingsDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Profile Form Settings'),
+      content: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FormBuilderCheckbox(
+              name: '',
+              title: Text('Display Subscriber Slider'),
+              initialValue: false, // TODO impl initial value
+              onChanged: (val) {}, // TODO impl onChanged
+            ),
+            FormBuilderCheckbox(
+              name: '',
+              title: Text('Display Playtime Slider'),
+              initialValue: false, // TODO impl initial value
+              onChanged: (val) {}, // TODO impl onChanged
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
