059c3ad7a9fbc52a65da617f9fc86ddac8ee7c6c
Eray Erdin <eraygezer.94@gmail.com>
2021-06-26 19:49:07 +0300

    added max threshold info to profile card interpolation field settings widget

--- lib/presentation/pages/settings/pages/card/profile/interpolation.part.dart
+++ lib/presentation/pages/settings/pages/card/profile/interpolation.part.dart
@@ -9,32 +9,44 @@ class _InterpolationOptionsSection extends StatelessWidget {
     return SectionComponent(
       label: 'Color Interpolation Options',
       children: [
-        FormBuilderDropdown<ProfileCardColorInterpolationField>(
-          name: '',
-          decoration: InputDecoration(labelText: 'Interpolation Field'),
-          items: ProfileCardColorInterpolationField.values
-              .map(
-                (e) => DropdownMenuItem(
-                  child: Text(e.toRepr()),
-                  value: e,
+        BlocBuilder<SettingsBloc, SettingsState>(
+          builder: (context, state) {
+            if (state is LoadedSettingsState) {
+              return FormBuilderDropdown<ProfileCardColorInterpolationField>(
+                name: '',
+                decoration: InputDecoration(
+                  labelText: 'Interpolation Field',
+                  helperText:
+                      'Maximum Predefined Threshold: ${state.settings.card.profileCard.colorInterpolationField.max}',
                 ),
-              )
-              .toList(),
-          initialValue:
-              settingsBloc.settings.card.profileCard.colorInterpolationField,
-          onChanged: (field) {
-            settingsBloc.add(
-              UpdateSettingsEvent(
-                settings: settingsBloc.settings.copyWith(
-                  card: settingsBloc.settings.card.copyWith(
-                    profileCard:
-                        settingsBloc.settings.card.profileCard.copyWith(
-                      colorInterpolationField: field,
+                items: ProfileCardColorInterpolationField.values
+                    .map(
+                      (e) => DropdownMenuItem(
+                        child: Text(e.toRepr()),
+                        value: e,
+                      ),
+                    )
+                    .toList(),
+                initialValue: settingsBloc
+                    .settings.card.profileCard.colorInterpolationField,
+                onChanged: (field) {
+                  settingsBloc.add(
+                    UpdateSettingsEvent(
+                      settings: settingsBloc.settings.copyWith(
+                        card: settingsBloc.settings.card.copyWith(
+                          profileCard:
+                              settingsBloc.settings.card.profileCard.copyWith(
+                            colorInterpolationField: field,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-            );
+                  );
+                },
+              );
+            }
+
+            return SizedBox();
           },
         ),
       ],
