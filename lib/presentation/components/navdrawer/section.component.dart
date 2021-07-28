bb11e6614d4dba8a9d1c55033116cfd306f8b0e3
Eray Erdin <eraygezer.94@gmail.com>
2021-07-28 09:19:08 +0300

    added NavDrawerSectionComponent

---
+++ lib/presentation/components/navdrawer/section.component.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 Eray Erdin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import 'package:flutter/material.dart';
+
+class NavDrawerSectionComponent extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+
+  const NavDrawerSectionComponent({
+    Key? key,
+    required this.title,
+    required this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        ...children
+      ],
+    );
+  }
+}
