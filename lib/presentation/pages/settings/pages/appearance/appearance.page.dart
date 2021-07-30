0c8254ff448a5c2c750340af872d6d34091418ab
Eray Erdin <eraygezer.94@gmail.com>
2021-07-30 11:38:08 +0300

    wip: designed appearance settings page

--- lib/presentation/pages/settings/pages/appearance/appearance.page.dart
+++ lib/presentation/pages/settings/pages/appearance/appearance.page.dart
@@ -5,18 +5,63 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:levelheadbrowser/presentation/components/colorpicker/colorpicker.component.dart';
 
 class AppearanceSettingsPage extends StatelessWidget {
-  const AppearanceSettingsPage({Key? key}) : super(key: key);
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  AppearanceSettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Appearance Settings'),
-      ),
-      body: Center(
-        child: Text('Hello from appearance settings.'),
+    return FormBuilder(
+      key: _formKey,
+      autovalidateMode: AutovalidateMode.always,
+      onChanged: () {},
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Appearance Settings'),
+          actions: [
+            IconButton(
+              onPressed: null, // TODO activate depending on state
+              icon: Icon(Icons.save),
+            ),
+          ],
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(10),
+          children: [
+            FormBuilderColorPicker(
+              labelText: 'Profile Card Minimum Color',
+              initialColor:
+                  Colors.red, // TODO set initial depending on settings
+              onChanged: (color) {}, // TODO change saveable state
+              name: 'profileMinColor',
+            ),
+            FormBuilderColorPicker(
+              labelText: 'Profile Card Maximum Color',
+              initialColor:
+                  Colors.green, // TODO set initial depending on settings
+              onChanged: (color) {}, // TODO change saveable state
+              name: 'profileMaxColor',
+            ),
+            FormBuilderColorPicker(
+              labelText: 'Level Card Minimum Color',
+              initialColor:
+                  Colors.red, // TODO set initial depending on settings
+              onChanged: (color) {}, // TODO change saveable state
+              name: 'levelMinColor',
+            ),
+            FormBuilderColorPicker(
+              labelText: 'Level Card Maximum Color',
+              initialColor:
+                  Colors.green, // TODO set initial depending on settings
+              name: 'levelMaxColor',
+              onChanged: (color) {}, // TODO change saveable state
+            ),
+          ],
+        ),
       ),
     );
   }
