3b9540f336de5781cd6a5855b989158031b118eb
Eray Erdin <eraygezer.94@gmail.com>
2021-07-30 14:09:50 +0300

    implemented initial values

--- lib/presentation/pages/settings/pages/appearance/appearance.page.dart
+++ lib/presentation/pages/settings/pages/appearance/appearance.page.dart
@@ -5,7 +5,9 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:levelheadbrowser/logic/settings/settings_bloc.dart';
 import 'package:levelheadbrowser/presentation/components/colorpicker/colorpicker.component.dart';
 
 class AppearanceSettingsPage extends StatelessWidget {
@@ -19,49 +21,57 @@ class AppearanceSettingsPage extends StatelessWidget {
       key: _formKey,
       autovalidateMode: AutovalidateMode.always,
       onChanged: () {},
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Appearance Settings'),
-          actions: [
-            IconButton(
-              onPressed: null, // TODO activate depending on state
-              icon: Icon(Icons.save),
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, state) {
+          if (state is LoadedSettingsState) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Appearance Settings'),
+                actions: [
+                  IconButton(
+                    onPressed: null, // TODO activate depending on state
+                    icon: Icon(Icons.save),
+                  ),
+                ],
+              ),
+              body: ListView(
+                padding: EdgeInsets.all(10),
+                children: [
+                  FormBuilderColorPicker(
+                    labelText: 'Profile Card Minimum Color',
+                    initialColor: state.settings.appearance.profileCardMinColor,
+                    onChanged: (color) {}, // TODO change saveable state
+                    name: 'profileMinColor',
+                  ),
+                  FormBuilderColorPicker(
+                    labelText: 'Profile Card Maximum Color',
+                    initialColor: state.settings.appearance.profileCardMaxColor,
+                    onChanged: (color) {}, // TODO change saveable state
+                    name: 'profileMaxColor',
+                  ),
+                  FormBuilderColorPicker(
+                    labelText: 'Level Card Minimum Color',
+                    initialColor: state.settings.appearance.levelCardMinColor,
+                    onChanged: (color) {}, // TODO change saveable state
+                    name: 'levelMinColor',
+                  ),
+                  FormBuilderColorPicker(
+                    labelText: 'Level Card Maximum Color',
+                    initialColor: state.settings.appearance.levelCardMaxColor,
+                    name: 'levelMaxColor',
+                    onChanged: (color) {}, // TODO change saveable state
+                  ),
+                ],
+              ),
+            );
+          }
+
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          ],
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(10),
-          children: [
-            FormBuilderColorPicker(
-              labelText: 'Profile Card Minimum Color',
-              initialColor:
-                  Colors.red, // TODO set initial depending on settings
-              onChanged: (color) {}, // TODO change saveable state
-              name: 'profileMinColor',
-            ),
-            FormBuilderColorPicker(
-              labelText: 'Profile Card Maximum Color',
-              initialColor:
-                  Colors.green, // TODO set initial depending on settings
-              onChanged: (color) {}, // TODO change saveable state
-              name: 'profileMaxColor',
-            ),
-            FormBuilderColorPicker(
-              labelText: 'Level Card Minimum Color',
-              initialColor:
-                  Colors.red, // TODO set initial depending on settings
-              onChanged: (color) {}, // TODO change saveable state
-              name: 'levelMinColor',
-            ),
-            FormBuilderColorPicker(
-              labelText: 'Level Card Maximum Color',
-              initialColor:
-                  Colors.green, // TODO set initial depending on settings
-              name: 'levelMaxColor',
-              onChanged: (color) {}, // TODO change saveable state
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
