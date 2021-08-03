93302feef9d697509e0a5dbc86ce0358febc5c0f
Eray Erdin <eraygezer.94@gmail.com>
2021-08-03 10:06:13 +0300

    added confirmation dialog

--- lib/presentation/pages/settings/pages/appearance/appearance.page.dart
+++ lib/presentation/pages/settings/pages/appearance/appearance.page.dart
@@ -9,6 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:levelheadbrowser/logic/settings/settings_bloc.dart';
 import 'package:levelheadbrowser/presentation/components/colorpicker/colorpicker.component.dart';
+import 'package:levelheadbrowser/presentation/pages/settings/components/confirmation_dialog/confirmation_dialog.component.dart';
 import 'package:levelheadbrowser/presentation/pages/settings/logic/settings_save/settings_save_cubit.dart';
 
 class AppearanceSettingsPage extends StatelessWidget {
@@ -28,67 +29,76 @@ class AppearanceSettingsPage extends StatelessWidget {
           autovalidateMode: AutovalidateMode.always,
           onChanged: () {},
           child: BlocBuilder<SettingsBloc, SettingsState>(
-            builder: (context, state) {
-              if (state is LoadedSettingsState) {
-                return Scaffold(
-                  appBar: AppBar(
-                    title: Text('Appearance Settings'),
-                    actions: [
-                      BlocBuilder<SettingsSaveCubit, Function()?>(
-                        builder: (context, state) {
-                          return IconButton(
-                            onPressed: state,
-                            icon: Icon(Icons.save),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                  body: ListView(
-                    padding: EdgeInsets.all(10),
-                    children: [
-                      FormBuilderColorPicker(
-                        labelText: 'Profile Card Minimum Color',
-                        initialColor:
-                            state.settings.appearance.profileCardMinColor,
-                        onChanged: (_) =>
-                            BlocProvider.of<SettingsSaveCubit>(context).emit(
-                          () => _saveSettings(context),
+            builder: (context, settingsState) {
+              if (settingsState is LoadedSettingsState) {
+                return BlocBuilder<SettingsSaveCubit, Function()?>(
+                  builder: (context, saveState) {
+                    return WillPopScope(
+                      onWillPop: saveState == null
+                          ? () async => true
+                          : () => _showConfirmationDialog(context),
+                      child: Scaffold(
+                        appBar: AppBar(
+                          title: Text('Appearance Settings'),
+                          actions: [
+                            IconButton(
+                              onPressed: saveState,
+                              icon: Icon(Icons.save),
+                            ),
+                          ],
                         ),
-                        name: 'profileMinColor',
-                      ),
-                      FormBuilderColorPicker(
-                        labelText: 'Profile Card Maximum Color',
-                        initialColor:
-                            state.settings.appearance.profileCardMaxColor,
-                        onChanged: (_) =>
-                            BlocProvider.of<SettingsSaveCubit>(context).emit(
-                          () => _saveSettings(context),
+                        body: ListView(
+                          padding: EdgeInsets.all(10),
+                          children: [
+                            FormBuilderColorPicker(
+                              labelText: 'Profile Card Minimum Color',
+                              initialColor: settingsState
+                                  .settings.appearance.profileCardMinColor,
+                              onChanged: (_) =>
+                                  BlocProvider.of<SettingsSaveCubit>(context)
+                                      .emit(
+                                () => _saveSettings(context),
+                              ),
+                              name: 'profileMinColor',
+                            ),
+                            FormBuilderColorPicker(
+                              labelText: 'Profile Card Maximum Color',
+                              initialColor: settingsState
+                                  .settings.appearance.profileCardMaxColor,
+                              onChanged: (_) =>
+                                  BlocProvider.of<SettingsSaveCubit>(context)
+                                      .emit(
+                                () => _saveSettings(context),
+                              ),
+                              name: 'profileMaxColor',
+                            ),
+                            FormBuilderColorPicker(
+                              labelText: 'Level Card Minimum Color',
+                              initialColor: settingsState
+                                  .settings.appearance.levelCardMinColor,
+                              onChanged: (_) =>
+                                  BlocProvider.of<SettingsSaveCubit>(context)
+                                      .emit(
+                                () => _saveSettings(context),
+                              ),
+                              name: 'levelMinColor',
+                            ),
+                            FormBuilderColorPicker(
+                              labelText: 'Level Card Maximum Color',
+                              initialColor: settingsState
+                                  .settings.appearance.levelCardMaxColor,
+                              name: 'levelMaxColor',
+                              onChanged: (_) =>
+                                  BlocProvider.of<SettingsSaveCubit>(context)
+                                      .emit(
+                                () => _saveSettings(context),
+                              ),
+                            ),
+                          ],
                         ),
-                        name: 'profileMaxColor',
                       ),
-                      FormBuilderColorPicker(
-                        labelText: 'Level Card Minimum Color',
-                        initialColor:
-                            state.settings.appearance.levelCardMinColor,
-                        onChanged: (_) =>
-                            BlocProvider.of<SettingsSaveCubit>(context).emit(
-                          () => _saveSettings(context),
-                        ),
-                        name: 'levelMinColor',
-                      ),
-                      FormBuilderColorPicker(
-                        labelText: 'Level Card Maximum Color',
-                        initialColor:
-                            state.settings.appearance.levelCardMaxColor,
-                        name: 'levelMaxColor',
-                        onChanged: (_) =>
-                            BlocProvider.of<SettingsSaveCubit>(context).emit(
-                          () => _saveSettings(context),
-                        ),
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 );
               }
 
@@ -105,4 +115,10 @@ class AppearanceSettingsPage extends StatelessWidget {
   }
 
   Future _saveSettings(BuildContext context) async {}
+
+  Future<bool> _showConfirmationDialog(BuildContext context) async {
+    showDialog<bool>(
+        context: context, builder: (context) => ConfirmationDialog());
+    return false;
+  }
 }
